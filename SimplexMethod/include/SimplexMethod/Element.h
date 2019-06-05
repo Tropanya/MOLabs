c03487b5274dee4a180fa0be2571ab22629721d9
tropanya <dima31121996@mail.ru>
2019-06-06 02:05:16 +0300

    [add] Добавил функции сравнения в класс Elemtent и недостающие арифметические операции.

--- SimplexMethod/include/SimplexMethod/Element.h
+++ SimplexMethod/include/SimplexMethod/Element.h
@@ -4,38 +4,61 @@
 #include <mpir.h>
 #include <mpirxx.h>
 /*============================================================================*/
+typedef mpq_class Fraction;
+/*============================================================================*/
 class Element
 {
 private:
-  mpq_class _free;
-  mpq_class _coefficient;
+  Fraction _free;
+  Fraction _coefficient;
+
+  bool _hasParameter;
+  bool _isCompute;
 public:
-  Element(
-    const mpq_class& free = 0,
-    const mpq_class& coefficient = 0);
+  explicit Element(
+    const Fraction& free = 0,
+    const Fraction& coefficient = 0);
+
+  Fraction Compute(const Fraction& parameter) const;
 
   Element& operator+=(const Element& other);
   Element& operator-=(const Element& other);
+  Element& operator*=(const Element& other);
+  Element& operator/=(const Element& other);
 
-  Element& operator+=(const mpq_class& other);
-  Element& operator-=(const mpq_class& other);
-  Element& operator*=(const mpq_class& other);
-  Element& operator/=(const mpq_class& other);
+  Element& operator+=(const Fraction& other);
+  Element& operator-=(const Fraction& other);
+  Element& operator*=(const Fraction& other);
+  Element& operator/=(const Fraction& other);
+
+  friend Element operator-(const Element& other);
 
   friend Element operator+(const Element& lft, const Element& rht);
   friend Element operator-(const Element& lft, const Element& rht);
+  friend Element operator*(const Element& lft, const Element& rht);
+  friend Element operator/(const Element& lft, const Element& rht);
 
-  friend Element operator+(const Element& lft, const mpq_class& rht);
-  friend Element operator-(const Element& lft, const mpq_class& rht);
-  friend Element operator*(const Element& lft, const mpq_class& rht);
-  friend Element operator/(const Element& lft, const mpq_class& rht);
+  friend Element operator+(const Element& lft, const Fraction& rht);
+  friend Element operator-(const Element& lft, const Fraction& rht);
+  friend Element operator*(const Element& lft, const Fraction& rht);
+  friend Element operator/(const Element& lft, const Fraction& rht);
 
-  friend Element operator+(const mpq_class& lft, const Element& rht);
-  friend Element operator-(const mpq_class& lft, const Element& rht);
-  friend Element operator*(const mpq_class& lft, const Element& rht);
+  friend Element operator+(const Fraction& lft, const Element& rht);
+  friend Element operator-(const Fraction& lft, const Element& rht);
+  friend Element operator*(const Fraction& lft, const Element& rht);
+  friend Element operator/(const Fraction& lft, const Element& rht);
+
+  friend bool operator<(const Element& lft, const Element& rht);
+  friend bool operator<=(const Element& lft, const Element& rht);
+  friend bool operator>(const Element& lft, const Element& rht);
+  friend bool operator>=(const Element& lft, const Element& rht);
 
   friend bool operator==(const Element& lft, const Element& rht);
   friend bool operator!=(const Element& lft, const Element& rht);
+
+  friend Element abs(const Element& other);
+
+  inline const Fraction& GetFree() const { return _free; }
 
   friend std::ostream& operator<<(
     std::ostream& stream,
--- SimplexMethod/src/Element.cpp
+++ SimplexMethod/src/Element.cpp
@@ -1,13 +1,27 @@
 #include "SimplexMethod/Element.h"
 /*============================================================================*/
 #include <cassert>
+#include <iostream>
 /*============================================================================*/
 Element::Element(
-  const mpq_class& free,
-  const mpq_class& coefficient):
+  const Fraction& free,
+  const Fraction& coefficient):
   _free(free),
-  _coefficient(coefficient)
-{  }
+  _coefficient(coefficient),
+  _hasParameter(false),
+  _isCompute(true)
+{
+  if (Fraction(0) != _coefficient)
+  {
+    _hasParameter = true;
+    _isCompute = false;
+  }
+}
+/*============================================================================*/
+Fraction Element::Compute(const Fraction& parameter) const
+{
+  return _free + (_coefficient * parameter);
+}
 /*============================================================================*/
 Element& Element::operator+=(const Element& other)
 {
@@ -25,21 +39,43 @@ Element& Element::operator-=(const Element& other)
   return (*this);
 }
 /*============================================================================*/
-Element& Element::operator+=(const mpq_class& other)
+Element& Element::operator*=(const Element& other)
+{
+  if (!this->_isCompute || !other._isCompute)
+    assert(false);
+
+  this->_free *= other._free;
+
+  return (*this);
+}
+/*============================================================================*/
+Element& Element::operator/=(const Element& other)
+{
+  if (!this->_isCompute || !other._isCompute)
+    assert(false);
+
+  assert(Fraction(0) != other._free);
+
+  this->_free /= other._free;
+
+  return (*this);
+}
+/*============================================================================*/
+Element& Element::operator+=(const Fraction& other)
 {
   this->_free += other;
 
   return (*this);
 }
 /*============================================================================*/
-Element& Element::operator-=(const mpq_class& other)
+Element& Element::operator-=(const Fraction& other)
 {
   this->_free -= other;
 
   return (*this);
 }
 /*============================================================================*/
-Element& Element::operator*=(const mpq_class& other)
+Element& Element::operator*=(const Fraction& other)
 {
   this->_free *= other;
   this->_coefficient *= other;
@@ -47,14 +83,21 @@ Element& Element::operator*=(const mpq_class& other)
   return (*this);
 }
 /*============================================================================*/
-Element& Element::operator/=(const mpq_class& other)
+Element& Element::operator/=(const Fraction& other)
 {
-  assert(mpq_class(0) != other);
+  assert(Fraction(0) != other);
 
   this->_free /= other;
   this->_coefficient /= other;
 
   return (*this);
+}
+/*============================================================================*/
+Element operator-(const Element& other)
+{
+  return Element(
+    -other._free,
+    -other._coefficient);
 }
 /*============================================================================*/
 Element operator+(const Element& lft, const Element& rht)
@@ -71,55 +114,112 @@ Element operator-(const Element& lft, const Element& rht)
     lft._coefficient - rht._coefficient);
 }
 /*============================================================================*/
-Element operator+(const Element& lft, const mpq_class& rht)
+Element operator*(const Element& lft, const Element& rht)
+{
+  if (!lft._isCompute || !rht._isCompute)
+    assert(false);
+
+  return Element(
+    lft._free * rht._free);
+}
+/*============================================================================*/
+Element operator/(const Element& lft, const Element& rht)
+{
+  if (!lft._isCompute || !rht._isCompute)
+    assert(false);
+
+  assert(Fraction(0) != rht._free);
+
+  return Element(
+    lft._free / rht._free);
+}
+/*============================================================================*/
+Element operator+(const Element& lft, const Fraction& rht)
 {
   return Element(
     lft._free + rht,
     lft._coefficient);
 }
 /*============================================================================*/
-Element operator-(const Element& lft, const mpq_class& rht)
+Element operator-(const Element& lft, const Fraction& rht)
 {
   return Element(
     lft._free - rht,
     lft._coefficient);
 }
 /*============================================================================*/
-Element operator*(const Element& lft, const mpq_class& rht)
+Element operator*(const Element& lft, const Fraction& rht)
 {
   return Element(
     lft._free * rht,
     lft._coefficient * rht);
 }
 /*============================================================================*/
-Element operator/(const Element& lft, const mpq_class& rht)
+Element operator/(const Element& lft, const Fraction& rht)
 {
-  assert(mpq_class(0) != rht);
+  assert(Fraction(0) != rht);
 
   return Element(
     lft._free / rht,
     lft._coefficient / rht);
 }
 /*============================================================================*/
-Element operator+(const mpq_class& lft, const Element& rht)
+Element operator+(const Fraction& lft, const Element& rht)
 {
   return Element(
     lft + rht._free,
     rht._coefficient);
 }
 /*============================================================================*/
-Element operator-(const mpq_class& lft, const Element& rht)
+Element operator-(const Fraction& lft, const Element& rht)
 {
   return Element(
     lft - rht._free,
     rht._coefficient);
 }
 /*============================================================================*/
-Element operator*(const mpq_class& lft, const Element& rht)
+Element operator*(const Fraction& lft, const Element& rht)
 {
   return Element(
     lft * rht._free,
     lft * rht._coefficient);
+}
+/*============================================================================*/
+Element operator/(const Fraction& lft, const Element& rht)
+{
+  if (!rht._isCompute)
+    assert(false);
+
+  assert(Fraction(0) != rht._free);
+
+  return Element(
+    lft / rht._free);
+}
+/*============================================================================*/
+bool operator<(const Element& lft, const Element& rht)
+{
+  Fraction l = lft.Compute(0);
+  Fraction r = rht.Compute(0);
+
+  return l < r;
+}
+/*============================================================================*/
+bool operator<=(const Element& lft, const Element& rht)
+{
+  Fraction l = lft.Compute(0);
+  Fraction r = rht.Compute(0);
+
+  return l <= r;
+}
+/*============================================================================*/
+bool operator>(const Element& lft, const Element& rht)
+{
+  return !(lft <= rht);
+}
+/*============================================================================*/
+bool operator>=(const Element& lft, const Element& rht)
+{
+  return !(lft < rht);
 }
 /*============================================================================*/
 bool operator==(const Element& lft, const Element& rht)
@@ -132,11 +232,19 @@ bool operator!=(const Element& lft, const Element& rht)
   return !(lft == rht);
 }
 /*============================================================================*/
+Element abs(const Element& other)
+{
+  if (other._isCompute)
+    return Element(abs(other._free));
+  else
+    assert(false);
+}
+/*============================================================================*/
 std::ostream& operator<<(
   std::ostream& stream,
   const Element& elemnt)
 {
-  if (mpq_class(0) != elemnt._coefficient)
+  if (Fraction(0) != elemnt._coefficient)
     stream << "(" << elemnt._free << "+(" << elemnt._coefficient << "t))";
   else
     stream << elemnt._free;
